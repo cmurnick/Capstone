a7a32e978b0dc96dc1843568908285b2491e03fc
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-12-05 18:08:20 -0600

    data pulls from search

---
+++ javascripts/controllers/SearchCtrl.js
@@ -0,0 +1,19 @@
+"use strict";
+
+app.controller("SearchCtrl", function($scope, EdamamService) {
+	$scope.recipes = [];
+
+	$scope.enterPush = (event) => {
+			if(event.keyCode === 13) {
+				console.log("event", event);
+				EdamamService.searchRecipes(event.target.value).then((results) =>{
+					console.log("results pull?", results);
+					$scope.recipes = results.data.hits;
+				console.log("scope results", $scope.recipes);
+				console.log("recipes?", results.data.hits);
+			}).catch((err) => {
+				console.log("error in searchMovies", err);
+			});
+			}
+		};
+});
